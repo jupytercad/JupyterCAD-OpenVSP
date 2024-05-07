17e623aa4543eb5cd31fb95198fb0ed591898c78
Duc Trung LE <leductrungxf@gmail.com>
2024-05-07 18:06:24 +0200

    Adopt precommit

--- jupytercad_openvsp/__init__.py
+++ jupytercad_openvsp/__init__.py
@@ -5,22 +5,18 @@ except ImportError:
     # in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
+
     warnings.warn("Importing 'jupytercad_openvsp' outside a proper installation.")
     __version__ = "dev"
 from .handlers import setup_handlers
 
 
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": "@jupytercad/jupytercad-openvsp"
-    }]
+    return [{"src": "labextension", "dest": "@jupytercad/jupytercad-openvsp"}]
 
 
 def _jupyter_server_extension_points():
-    return [{
-        "module": "jupytercad_openvsp"
-    }]
+    return [{"module": "jupytercad_openvsp"}]
 
 
 def _load_jupyter_server_extension(server_app):
